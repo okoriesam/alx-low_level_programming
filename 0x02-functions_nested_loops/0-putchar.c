0f73e76627acc883e1580e33521f3fb1c11e6a85
Ugochukwu <okoriesamuel253@gmail.com>
2023-03-21 19:10:31 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <main.h>
+#include <stdio.h>
+/**
+ * main - A program that print _putchar
+ * Result: 0(success)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar("\n");
+	return (0);
+}
