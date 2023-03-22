1fa1601da77c069f57ff2e50aca1d4336c72ec9c
Ugochukwu <okoriesamuel253@gmail.com>
2023-03-22 06:48:36 -0700

    update on 4_isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,10 @@
 #include "main.h"
+
 /**
- *4_isalpha - a function that checks for alphabetic character.
+ *_isalpha - a function that checks for alphabetic character.
  * Return : 1 for alphabetic character or 0 for anything else
  */
+
 int _isalpha(int c)
 {
 if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
