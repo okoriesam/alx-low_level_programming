cceee3a55c4a9bbb9fba95d295b02493c2e6af57
Ugochukwu <okoriesamuel253@gmail.com>
2023-03-22 04:12:51 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - print alphabe
+ * return :void
+ */
+   void	print_alphabet(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	_putchar(ch);
+	_putchar('\n');
+}
